10816e220f6e77891e33860b04be3ab4f0dc29be
LicoCode <30888949+LicoCode@users.noreply.github.com>
2024-04-19 22:08:06 +0800

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -11,7 +11,7 @@ namespace CamstarServiceClient
             //为CamstarServiceClient配置CamstarAppServer信息
             ServiceConfiguration.Host = "localhost";
             ServiceConfiguration.Port =443;
-            ServiceConfiguration.DefaultPassword = "Tenda.123";
+            ServiceConfiguration.DefaultPassword = "******";
             ServiceConfiguration.DefaultUser = "CamstarAdmin";
 
 
@@ -25,7 +25,7 @@ namespace CamstarServiceClient
             start.CurrentStatusDetails = new CurrentStatusStartDetails();
             start.CurrentStatusDetails.Workflow = new WorkflowRef("Workflow_A", null, true);
             start.Details.Owner = new OwnerRef("Owner_A");
-            var sessionName = ServiceClient.Login("CamstarAdmin", "Tenda.123");
+            var sessionName = ServiceClient.Login("CamstarAdmin", "******");
             var result = new ServiceClient(sessionName).Submit(start);
         }
     }
